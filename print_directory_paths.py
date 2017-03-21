a90dd93283acab7f6d3a110a7dfa54e9a953d4f9
Ishitva Goel <ishitva.goel@yahoo.co.in>
2017-03-21 22:11:11 +0530

    Script to print Directory Structure.

---
+++ print_directory_paths.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+ROOT_DIR = '.'  # The directory to start from. Present directory in this case.
+TRAVERSAL_BOOLEAN_MAP = {
+  'topdown': True,
+  'bottomup': False
+}
+
+def PrintFileStructure():
+  """Prints the file structure in the present directory."""
+
+  try:
+    for dir_name, sub_dir_list, file_list in os.walk(
+        ROOT_DIR, topdown=TRAVERSAL_BOOLEAN_MAP[sys.argv[1]]):
+      print 'Found Directory: %s' % (dir_name)
+      for file_name in file_list:
+        print '\t%s' % (file_name)
+  except IndexError as e:
+    print 'Traversal order not specified. Error: %s' % (e)
+
+
+def main():
+  PrintFileStructure()
+
+
+if __name__ == '__main__':
+  main()
+
+
+
